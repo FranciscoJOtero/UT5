566fd109810bd816f0364c7d032bc95b78f4db2e
Fran <fjoteroherrero@infomm.local>
2020-01-27 20:06:13 +0100

    Version 1.0 de la rama master Sesion 33

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import Tabla from './Tabla'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+    render() {
+
+        return (
+
+            <div className="App">
+                <h1> tabla creada mediante un componente de clase que no utiliza componentes simples </h1>
+                <Tabla/>
+            </div>
+
+        )
+
+    }
+
 }
 
-export default App;
+export default App
